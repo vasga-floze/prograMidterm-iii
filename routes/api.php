61c151ee93c7b83b09666cf20935d2304d256309
Kilmar22 <72371533+Kilmar22@users.noreply.github.com>
2021-06-13 01:18:20 -0600

    Se agregan las rutas para el api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +18,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//rutas para API
+//listar
+Route::get('recetas', 'App\Http\Controllers\RecetasController@index');
+//listar por id
+Route::get('receta/{id}', 'App\Http\Controllers\RecetasController@show');
+//guardar
+Route::post('receta', 'App\Http\Controllers\RecetasController@store');
+//update
+Route::put('receta', 'App\Http\Controllers\RecetasController@store');
+//eliminar
+Route::delete('receta/{id}', 'App\Http\Controllers\RecetasController@destroy');
