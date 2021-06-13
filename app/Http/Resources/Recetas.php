bd5160b7e3123d43cfc20932a02b56b914bc7185
Kilmar22 <72371533+Kilmar22@users.noreply.github.com>
2021-06-13 01:18:01 -0600

    Se crea modelo y controlador para recetas

---
+++ app/Http/Resources/Recetas.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Recetas extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+       // return parent::toArray($request);
+       return[
+           'id'=>$this->id,
+           'nombre'=>$this->nombre,
+           'ingredientes'=>$this->ingredientes,
+           'tpreparacion'=>$this->tpreparacion,
+           'dificultad'=>$this->dificultad,
+           'imagen'=>$this->imagen,
+
+       ];
+    }
+}
