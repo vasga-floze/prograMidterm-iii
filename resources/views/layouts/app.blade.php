31c0853e009bcacb01f3be9db9320e526462fb9f
Kilmar22 <72371533+Kilmar22@users.noreply.github.com>
2021-06-13 21:27:16 -0600

    actualiza rutas a home

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,10 +23,10 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <div class="pr-3" href="{{ url('/') }}">
+                <div class="pr-3" href="{{ url('/home') }}">
                     <img class="img-responsive" src="{{asset('images/logoTalkfood.png')}}" width="50" height="50">
                 </div>    
-                    <a class="navbar-brand" href="{{ url('/') }}">
+                    <a class="navbar-brand" href="{{ url('/home') }}">
                         TALKFOOD
                     </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
