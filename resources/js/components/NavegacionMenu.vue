713fa773ee0141c990df8a4e34844c26b7afff2b
@vasga-floze <gabriela.floze@gmail.com>
2021-06-13 22:31:31 -0600

    actualiza rutas de navbar secundario

--- resources/js/components/NavegacionMenu.vue
+++ resources/js/components/NavegacionMenu.vue
@@ -1,11 +1,11 @@
 <template>
         <div class="container-fluid">
             <form class="form-inline my-4 my-lg-0">
-                <a class="nav-link mx-auto" href=""><span>Biografía</span></a>
+                <a class="nav-link mx-auto" href="/perfil"><span>Biografía</span></a>
 
-                <a class="nav-link mx-auto" href=""><span>Favoritos</span></a>
+                <a class="nav-link mx-auto" href="favorite"><span>Favoritos</span></a>
 
-                <a class="nav-link mx-auto" href=""><span>Recetario</span></a>
+                <a class="nav-link mx-auto" href="/recetario"><span>Recetario</span></a>
             </form>
         </div>
 </template>
