1b899c24b7f3d725a366dacbcb28daa269ae92f8
Kilmar22 <72371533+Kilmar22@users.noreply.github.com>
2021-06-13 21:18:45 -0600

    Se agrega logo a la vista de home

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>TALKFOOD</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -23,9 +23,12 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/home') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
+                <div class="pr-3" href="{{ url('/') }}">
+                    <img class="img-responsive" src="{{asset('images/logoTalkfood.png')}}" width="50" height="50">
+                </div>    
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        TALKFOOD
+                    </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
