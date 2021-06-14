380db53d50f1fdd33b0eed7e100c418398b8e61e
@vasga-floze <gabriela.floze@gmail.com>
2021-06-13 20:11:02 -0600

    agrega componentes vue y plantilla para perfil

---
+++ resources/js/components/NavegacionMenu.vue
@@ -0,0 +1,11 @@
+<template>
+        <div class="container-fluid">
+            <form class="form-inline my-4 my-lg-0">
+                <a class="nav-link mx-auto" href=""><span>Biografía</span></a>
+
+                <a class="nav-link mx-auto" href=""><span>Favoritos</span></a>
+
+                <a class="nav-link mx-auto" href=""><span>Recetario</span></a>
+            </form>
+        </div>
+</template>
